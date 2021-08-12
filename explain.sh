83d395e4e5c931a95765ecb906ce83e85660493f
Faelin Landy <flandy@rosettastone.com>
2021-08-12 17:35:51 -0400

    updated

--- explain.sh
+++ explain.sh
@@ -1,37 +1,61 @@
-#!/bin/sh
+#!/bin/bash
 
 ###
-alias EXPLAINER="$((command -v bat &>/dev/null) && echo "bat -l'sh'" || echo "cat")"
+# shellcheck disable=SC2139
+alias READER="$(  (command -v bat &>/dev/null) && echo "bat -l'sh'" || echo "cat"  )"
 ###
 export EXPANSION_ON_SPACE=false
 export EXPANSION_FILTER=()
-###
+typeset -ga EXPANSION_FILTER
+##
 RED='\033[1;31m'
 GREEN='\033[0;32m'
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 ###
 
+
 select-from-list () {
-  if (command -v fzf)
-  then
-    fzf --height 40% --reverse <<<$@
+  # local IFS=$'\n'
+  list=()
+  if [ "$#" -gt 0 ]; then
+    for line in "$@";
+    do
+      list+=("$line");
+    done
   else
-    COLUMNS=1
-    PS3="option: "
-    select item
+    # while read -r line ; do
+    #   list+=("$line")
+    # done
+  fi
+
+  # if (command -v fzf >/dev/null 2>&1)
+  # then
+  #     printf '%s\n' "${list[@]}" | fzf --height 40% --reverse
+  # else
+    length=0
+    for x in ${list[@]}
+    do
+       if [ ${#x} -gt $length ]
+       then
+          length=${#x}
+       fi
+    done
+    
+    local COLUMNS=$length
+    local PS3="option: "
+    select item in ${list[@]}
     do
       echo "$item"
-      break;
+      return;
     done
-  fi
+  # fi
 }
 
 find-alias () {
   echo -n "searching..."
 
   source="$(${$(ps -cp "$$" -o command="")##-} -ixlc : 2>&1 | grep "alias '$1=.*'" | tail -1)"
-  echo "\r$source"
   read file line <<< $(echo $source | sed -E "s/^\+(.*\/.*):([0-9]+)> alias '$1=.*'/\1 \2/")
   statement="$(cat $file | sed -n "${line}p")"
   
@@ -39,17 +63,17 @@ find-alias () {
   echo "${PURPLE}$file\n${GREEN}$line${NC}:${RED}$statement${NC}"
 }
 
-
-## see what aliases and shortcuts mean
 explain () {
-  command=$@[-1]
-  unset $@[-1]
+  args=($@)
+  command=${args[-1]}
+  unset $args[-1]
 
   [[ ! $(command -v $command) ]] && echo "command not found: '$command'" && return 0
 
   if [[ $(type -a $command | wc -l) -gt 1 ]]
   then    
-    target="$(IFS=$'\n'; select-from-list $(type -a "$command") )"
+    # zsh variant:  target="$( IFS=$'\n'; select-from-list $(type -a "$command") )"
+    # bash variant:  target="$( IFS=$'\n' select-from-list $(type -a "$command") )"
   else
     target="$(type -a "$command")"
   fi
@@ -63,16 +87,16 @@ explain () {
       ;;
     command\ *)
       # echo "command!"
-      EXPLAINER $(command -v "$command") $@
+      READER $(command -v "$command") $args
       ;;
     function\ *)
       # echo "function!"
-      type -f "$command" | EXPLAINER $@
+      type -f "$command" | READER $args
       ;;
     hash\ *)
       echo "hash!"
       output=$(hash -m "$1")
-      EXPLAINER ${output##*=} $@
+      READER ${output##*=} $args
       ;;
     shell\ builtin)
       # echo "builtin!"
@@ -84,7 +108,7 @@ explain () {
       then
         man $command 2>/dev/null || $command --help
       else
-        EXPLAINER $target
+        READER $target
       fi
       ;;
     *)
@@ -95,7 +119,7 @@ explain () {
 }
 
 
-# zle 
+# used for zle widgets
 expand-or-explain () {
   local lh="$(echo $LBUFFER | grep -oE "[-[:alnum:]]+$")" # matches a word at the end of LBUFFER
   local rh="$(echo $RBUFFER | grep -oE "^[-[:alnum:]]+")" # matches a word at the beginning of RBUFFER
@@ -115,6 +139,7 @@ expand-or-explain () {
 }
 
 
+# used for zle widgets
 expand-alias () {
   local target="$(echo $LBUFFER | grep -oE "[-_[:alnum:]]+$")" # matches a word at the end of LBUFFER
 
