1901a0d2ef437bfb58ae1236eaf47b710760fedd
Faelin Landy <flandy@rosettastone.com>
2021-08-13 15:54:33 -0400

    temporarily disable ^E shortcut

--- import_explain.zsh
+++ import_explain.zsh
@@ -1,18 +1,14 @@
 #!/bin/zsh
 
-echo "$(dirname "$(dirname "$(readlink -F "$0")")")"
-_explain_path="$(realpath "${0:A}/./")/explain.sh"
+_explain_path="$(dirname "$0")/explain.sh"
 
 source "$_explain_path"
-
-
-
 
 zle -N expand-or-explain
 bindkey '^E' expand-or-explain
 
 zle -N expand-alias
-bindkey ' '    expand-alias
+# bindkey ' '    expand-alias
 bindkey '^ '   magic-space          # control-space to bypass completion
 bindkey -M isearch " "  magic-space # normal space during searches
 
