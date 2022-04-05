eac58cbf18885a120344a43d1ebc1e169a43b94e
Faelin Landy <faelin@umich.edu>
2022-04-05 14:03:13 -0400

    created zsh plugin
    moved zsh plugin functionality from explain to explain.plugin.zsh

--- explain.plugin.zsh
+++ explain.plugin.zsh
@@ -1,8 +1,43 @@
 #!/bin/zsh
 
-_explain_path="$(dirname "$0")/explain.sh"
 
-source "$_explain_path"
+# used for zle widgets
+expand-or-explain () {
+  local lh="$(echo $LBUFFER | grep -oE "[-[:alnum:]]+$")" # matches a word at the end of LBUFFER
+  local rh="$(echo $RBUFFER | grep -oE "^[-[:alnum:]]+")" # matches a word at the beginning of RBUFFER
+  [ -z "$lh$rh" ] && local lh="$(echo $LBUFFER | grep -oE "[-[:alnum:]]+" | tail -1)"
+  local target="$lh$rh"
+
+  if [[ `type -w "$target"` == *": alias" ]]
+  then
+    unset 'functions[_expand-aliases]'
+    functions[_expand-aliases]=$target
+    (($+functions[_expand-aliases])) &&
+      BUFFER="${LBUFFER/%$lh}${functions[_expand-aliases]#$'\t'}${RBUFFER/#$rh}" &&
+      CURSOR=$#BUFFER
+  else
+    explain $(echo $target | cut -d' ' -f1) --pager='less -R'
+  fi
+}
+
+
+# used for zle widgets
+expand-alias () {
+  local target="$(echo $LBUFFER | grep -oE "[-_[:alnum:]]+$")" # matches a word at the end of LBUFFER
+
+  if [[ $EXPANSION_ON_SPACE == 'true' &&
+        -z ${EXPANSION_FILTER[(re)$target]} &&
+        `type -w "$target" 2>/dev/null` == *": alias"
+     ]]
+  then
+    unset 'functions[_expand-aliases]'
+    functions[_expand-aliases]=$target
+    (($+functions[_expand-aliases])) &&
+      BUFFER="${functions[_expand-aliases]#$'\t'}" &&
+      CURSOR=$#BUFFER
+  fi
+  zle magic-space
+}
 
 zle -N expand-or-explain
 bindkey '^E' expand-or-explain
